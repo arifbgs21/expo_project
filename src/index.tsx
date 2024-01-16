80a5d7af32a3e9a8430642971f6f59bc2d6734e6
unknown <arifbagus791@gmail.com>
2024-01-16 19:24:46 +0700

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import './index.css'
+
+import ReactDOM from 'react-dom/client'
+
+import { AppContainer } from './components/app-container'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(<AppContainer />)
